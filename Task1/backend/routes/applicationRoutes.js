5d09e869e6c91522ee0306fa824eb994f7d040a8
Ayush Rawat <ayushrf@gmail.com>
2026-03-06 08:53:59 +0530

    Update applicationRoutes.js

--- Task1/backend/routes/applicationRoutes.js
+++ Task1/backend/routes/applicationRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { applyToJob, getApplicationsForEmployer } from "../controllers/applicationController.js";
+import { applyToJob, getApplicationsForEmployer, getApplicationsForCandidate } from "../controllers/applicationController.js";
 import { protect } from "../middleware/authMiddleware.js";
 import upload from "../middleware/uploadMiddleware.js";
 
@@ -8,5 +8,7 @@ const router = express.Router();
 router.post("/:jobId", protect, upload.single("resume"), applyToJob);
 
 router.get("/employer", protect, getApplicationsForEmployer);
+
+router.get("/candidate", protect, getApplicationsForCandidate);
 
 export default router;
